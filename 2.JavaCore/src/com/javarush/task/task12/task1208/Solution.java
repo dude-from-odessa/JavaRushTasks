dd83abc9b2592133d09a7670b1e9ecb3a2810f23
dude-from-odessa <dude.from.odessa@gmail.com>
2023-05-28 18:46:05 +0300

    MineswepperGame 7 Stage

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+
+    public static void print(int x) {
+
+    }
+
+    public static void print(String x) {
+
+    }
+
+    public static void print(char x) {
+
+    }
+
+    public static void print(long x) {
+
+    }
+
+    public static void print(short x) {
+
+    }
+}
